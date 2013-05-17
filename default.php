2854ed20804b23dacebeba7a36328e88c9ce90a4
Jesse Daugherty <jessedaugherty@gmail.com>
2013-05-17 08:50:20 -0400

    init

---
+++ default.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Arbrr Labs</title>
+        <meta name="description" content="Arbrr Labs - Web and Software Development firm.">
+		<link href='http://fonts.googleapis.com/css?family=Arvo:400,700|Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+        <div id="follownav" class="hide"><div class="follownavwrap"><a href="#"><img src="img/favicon.png" width="50px" height="41px" class="logo"></a>
+                <nav>
+                	<ul class="jsnav">
+                        <li><a href="#teamanc"><p>The Team</p></a></li>
+                    	<li><a href="#workanc"><p>The Work</p></a></li>
+                        <li><a href="#contactanc"><p>Contact</p></a></li>
+
+                    </ul>
+                </nav></div></div>
+        <div class="topstrip">
+        	<header class="mainheader">
+            	<a href="#"><div class="logo">ARBRR LABS</div></a>
+                <nav>
+                	<ul class="jsnav">
+                         <li><a href="#teamanc"><p>The Team</p></a></li>
+                        <li><a href="#workanc"><p>The Work</p></a></li>
+                        <li><a href="#contactanc"><p>Contact</p></a></li>
+                    </ul>
+                </nav>
+                <h1><img src="img/arbrr.png"></h1>
+                <h2>Building your dreams<br> in Ann Arbor, MI</h2>
+            </header>
+            <div class="ctastrip">
+            	<div class="ctahold">
+                <a href="#" class="registercta"><p>Get in Touch</p><p>Let's talk about your idea</p></a>
+            </div>
+            </div>
+        </div>
+        <div class="site">
+		<div class="whatwrap" id="teamanc">
+        	<div class="whatstrip">
+           		<section class="textcontent">
+                	<h1>The Team</h1>
+                    <p>A group of talented developers and designers @ University of Michigan. We decided to combine forces so we could take on larger, more inspiring projects. Members of our team have worked at Apple, Google, Amazon, Lorem, Ipsum, and more.</p>
+                </section>
+                <section class="speaker">
+                    <div class="bgbox">
+                    </div>
+                </section>
+            </div>
+        </div>
+		<div class="whenwrap" id="workanc">
+        	<div class="whenstrip">
+           		<div class="sectionTop">
+                	<h1>The Work</h1>
+                    <p>Take a look at some of the apps and sites we've built in the past.  We've worked with clients big and small, and have never left one unsatisfied.</p>
+                </div>
+
+                <section class="meetupbox">
+                	<h2>Loco</h2>
+                    <p>We created the iOS app for Market Loco, a startup making a marketplace for college students.</p>
+					<a href="#">View on App Store</a>
+                </section>
+                <section class="meetupbox">
+                    <h2>Pear</h2>
+                    <p>We created the iOS app for Market Loco, a startup making a marketplace for college students.</p>
+                    <a href="#">View on App Store</a>
+                </section>
+                <section class="meetupbox">
+                    <h2>Lorem</h2>
+                    <p>We created the iOS app for Market Loco, a startup making a marketplace for college students.</p>
+                    <a href="#">View on App Store</a>
+                </section>
+                <section class="meetupbox">
+                    <h2>Ipsum</h2>
+                    <p>We created the iOS app for Market Loco, a startup making a marketplace for college students.</p>
+                    <a href="#">View on App Store</a>
+                </section>
+            </div>
+        </div>
+        <div class="contactwrap" id="contactanc">
+            <div class="whenstrip">
+                <div class="sectionTop">
+                    <h1>Contact Us</h1>
+                    <p>Want our next project to be your app? Get in touch.</p>
+                </div>
+
+            </div>
+        </div>
+        <div class="footerwrap">
+        	<footer class="mainfooter">
+                <p>&copy; Arbrr Labs, LLC 2013</p>
+            </footer>
+        </div>
+        </div>
+       
+        </div>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script type="text/javascript" src="jquery.easing.1.3.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+    </body>
+</html>
